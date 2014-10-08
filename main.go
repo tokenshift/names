14226546ff8e41cb720a932fd4254227133d76d1
Nathan Clark <Nathan.Clark@tokenshift.com>
2014-10-08 18:01:05 -0400

    Inline tags and dropping the name DB.

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	db := parseNameFile("test.names")
-
-	db.ForEach(func (name TaggedName) {
-		fmt.Println(name)
-	})
+	parseNameFile("test.names")
 }
--- name_file_parser.go
+++ name_file_parser.go
@@ -9,85 +9,48 @@ import (
 	. "github.com/prataprc/goparsec"
 )
 
-type NameDB interface {
-	// Add a name to the DB.
-	Add(TaggedName)
-
-	// Enumerates all names in the DB.
-	Each() (<-chan TaggedName)
-
-	// Applies a function to every name in the DB.
-	ForEach(func (TaggedName))
-}
-
-type nameDB map[string]TaggedName
-
-func (db nameDB) Add(n TaggedName) {
-	if name, ok := db[n.Name]; ok {
-		name = TaggedName {
-			n.Name,
-			append(name.Tags, n.Tags...),
-		}
-		db[n.Name] = name
-	} else {
-		db[n.Name] = n
-	}
-}
-
-func (db nameDB) Each() (<-chan TaggedName) {
-	out := make(chan TaggedName)
-
-	go func() {
-		for _, name := range(db) {
-			out <- name
-		}
-		close(out)
-	}()
-
-	return out
-}
-
-func (db nameDB) ForEach(f func(TaggedName)) {
-	for name := range(db.Each()) {
-		f(name)
-	}
-}
-
 type TaggedName struct {
 	Name string
 	Tags []string
 }
 
 func (n TaggedName) String() string {
-	return fmt.Sprintf("%s: %s", n.Name, strings.Join(n.Tags, ", "))
+	if len(n.Tags) > 0 {
+		return fmt.Sprintf("%s: %s", n.Name, strings.Join(n.Tags, ", "))
+	} else {
+		return n.Name
+	}
 }
 
-
-/* Instead of constructing an AST, the individual parsers return nil and update
- * a global database of names+tags as a side effect.
- */
-
 var tagStack [][]string
-var currentDB NameDB
+func tags() []string {
+	set := make(map[string]bool)
+	for _, tags := range(tagStack) {
+		for _, tag := range(tags) {
+			set[tag] = true
+		}
+	}
 
-func parseNameFile(filename string) NameDB {
+	distinct := make([]string, 0, len(set))
+	for tag, _ := range(set) {
+		distinct = append(distinct, tag)
+	}
+	return distinct
+}
+
+func parseNameFile(filename string) {
 	buffer, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	db := nameDB(make(map[string]TaggedName))
-
-	currentDB = db
 	tagStack = make([][]string, 0)
 	scanner := NewScanner(buffer)
 	parseBlockContents(scanner)
-	currentDB = nil
-
-	return db
 }
 
+// Block = TagList BlockContents '}'
 func parseBlock(s Scanner) (ParsecNode, Scanner) {
 	return And(func (ns []ParsecNode) ParsecNode {
 		tagStack = tagStack[:len(tagStack)-1]
@@ -95,6 +58,7 @@ func parseBlock(s Scanner) (ParsecNode, Scanner) {
 	}, parseTagList, parseBlockContents, rbrace)(s)
 }
 
+// TagList = TagListStart* TagListEnd
 func parseTagList(s Scanner) (ParsecNode, Scanner) {
 	inits := Kleene(func (ns []ParsecNode) ParsecNode {
 		tags := make([]string, len(ns))
@@ -111,53 +75,87 @@ func parseTagList(s Scanner) (ParsecNode, Scanner) {
 	}, inits, parseTagListEnd)(s)
 }
 
+// TagListStart = ident ','
 func parseTagListStart(s Scanner) (ParsecNode, Scanner) {
 	return And(func (ns []ParsecNode) ParsecNode {
 		return strings.TrimSpace(ns[0].(*Terminal).Value)
 	}, ident, comma)(s)
 }
 
+// TagListEnd = ident '{'
 func parseTagListEnd(s Scanner) (ParsecNode, Scanner) {
 	return And(func (ns []ParsecNode) ParsecNode {
 		return strings.TrimSpace(ns[0].(*Terminal).Value)
 	}, ident, lbrace)(s)
 }
 
+// BlockContents = BlockContent*
 func parseBlockContents(s Scanner) (ParsecNode, Scanner) {
 	return Kleene(func (ns []ParsecNode) ParsecNode {
 		return struct{}{}
 	}, parseBlockContent)(s)
 }
 
+// BlockContent = comment | Block | Name
 func parseBlockContent(s Scanner) (ParsecNode, Scanner) {
 	return OrdChoice(func (ns []ParsecNode) ParsecNode {
-		if term, ok := ns[0].(*Terminal); ok && term.Name == "IDENT" {
-			tagSet := make(map[string]bool)
-			for _, tags := range(tagStack) {
-				for _, tag := range(tags) {
-					tagSet[tag] = true
-				}
-			}
-
-			tags := make([]string, 0, len(tagSet))
-			for tag, _ := range(tagSet) {
-				tags = append(tags, tag)
-			}
-			currentDB.Add(TaggedName {
-				Name: term.Value,
-				Tags: tags,
-			})
+		if name, ok := ns[0].(TaggedName); ok {
+			name.Tags = append(name.Tags, tags()...)
+			fmt.Println(name)
 		}
 		return struct{}{}
-	}, comment, parseBlock, ident)(s)
+	}, comment, parseBlock, parseName)(s)
+}
+
+// Name = ident [InlineTags]
+func parseName(s Scanner) (ParsecNode, Scanner) {
+	inlineTags := Maybe(func (ns []ParsecNode) ParsecNode {
+		if ns == nil {
+			return []string {}
+		} else {
+			return ns[0].([]string)
+		}
+	}, parseInlineTags)
+
+	return And(func (ns []ParsecNode) ParsecNode {
+		if tags, ok := ns[1].([]string); ok {
+			return TaggedName {
+				Name: ns[0].(*Terminal).Value,
+				Tags: tags,
+			}
+		} else {
+			return TaggedName {
+				Name: ns[0].(*Terminal).Value,
+			}
+		}
+	}, ident, inlineTags)(s)
+}
+
+// InlineTags = ':' TagListStart* ident
+func parseInlineTags(s Scanner) (ParsecNode, Scanner) {
+	inits := Kleene(func (ns []ParsecNode) ParsecNode {
+		tags := make([]string, len(ns))
+		for i, n := range(ns) {
+			tags[i] = n.(string)
+		}
+		return tags
+	}, parseTagListStart)
+
+	return And(func (ns []ParsecNode) ParsecNode {
+		return append(ns[1].([]string), ns[2].(*Terminal).Value)
+	}, colon, inits, ident)(s)
 }
 
 func ident(s Scanner) (ParsecNode, Scanner) {
-	return Token(`^[^,{}\r\n]+`, "IDENT")(s)
+	return Token(`^[^,{}:\r\n]+`, "IDENT")(s)
 }
 
 func comma(s Scanner) (ParsecNode, Scanner) {
 	return Token(`^,`, "COMMA")(s)
+}
+
+func colon(s Scanner) (ParsecNode, Scanner) {
+	return Token(`^:`, "COLON")(s)
 }
 
 func lbrace(s Scanner) (ParsecNode, Scanner) {
